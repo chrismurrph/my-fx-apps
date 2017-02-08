5c66413da049d54e7429e4c1593280e10f583c6c
Chris Murphy <chris@strandz.org>
2017-02-08 12:42:47 +1030

    Tried doing transition. Didn't work b/c I can't get the node out

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [halgari/fn-fx "0.3.0-SNAPSHOT"]
                  [org.clojure/data.csv "0.1.3"]
                  [proto-repl "0.3.1"]]
--- src/my_trys/scene_graph_demo.clj
+++ src/my_trys/scene_graph_demo.clj
@@ -1,24 +1,49 @@
 (ns my-trys.scene-graph-demo
   (:require [fn-fx.fx-dom :as dom]
-            [fn-fx.controls :as ui]
-            )
-  (:import [javafx.animation TranslateTransition]))
+            [fn-fx.controls :as ui])
+  (:import [javafx.animation TranslateTransition ParallelTransition Timeline]
+           [javafx.util Duration]))
 
 (def black (ui/color :red 0 :blue 0 :green 0))
 (def blue (ui/color :red 0 :blue 1 :green 0))
 
-(def translate (ui/translate-transition
-                 :to-x 390
-                 :to-y 390))
+(defn create-translate []
+  (let [trans (TranslateTransition. (Duration/millis 750))]
+    (doto trans
+      (.setToX 390)
+      (.setToY 390))))
 
-(def r (ui/rectangle
+;;
+;; FillTransition fill = new FillTransition(Duration.millis(750));
+;; fill.setToValue(Color.RED);
+
+
+
+;; RotateTransition rotate = new RotateTransition(Duration.millis(750));
+;; rotate.setToAngle(360);
+
+;; ScaleTransition scale = new ScaleTransition(Duration.millis(750));
+;; scale.setToX(0.1);
+;; scale.setToY(0.1);
+;;
+
+(defn run-p-transition [rect translate]
+  (assert rect)
+  (assert translate)
+  (let [p-transition (ParallelTransition. rect (into-array [translate]))]
+    (doto p-transition
+      (.setCycleCount Timeline/INDEFINITE)
+      (.setAutoReverse true)
+      (.play))))
+
+(def rect (ui/rectangle
          :x 25
          :y 25
          :width 250
          :height 250
          :fill blue))
 
-(def transition (ui/parallel-transition r translate))
+;(def transition (ui/parallel-transition r translate))
 
 (defn -main []
   (let [u (ui/stage
@@ -30,8 +55,11 @@
                      :height 500
                      :root (ui/group
                              :rotate 45
-                             :children [r])))
+                             :children [rect])))
 
         handler-fn (fn [evt]
                      (println "Received Event: " evt))]
+    ;; Won't work b/c I can't get the original component out of rect
+    ;(println "rect: " (.getNode rect))
+    ;(run-p-transition (:dom-node rect) (create-translate) #_(create-fill) #_(create-rotate) #_(create-scale))
     (dom/app u nil)))
